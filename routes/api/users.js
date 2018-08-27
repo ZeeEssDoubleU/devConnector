2d3095931cddd9e613a7ce64630d7b0d73fb32ad
Zak Williams <zak.williams2287@gmail.com>
2018-08-27 15:44:56 -0400

    Created user login POST route.  Set up email/password verification logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,4 +49,31 @@ router.post('/register', (req, res) => {
       })
 });
 
+// @route - GET api/users/login
+// @desc - login user
+// @access - public
+router.post('/login', (req, res) => {
+   const email = req.body.email;
+   const password = req.body.password;
+
+   // find user by email
+   User.findOne({ email })
+      .then(user => {
+         // check for user
+         if (!user) {
+            return res.status(404).json({ email: 'User not found' });
+         }
+
+         // check password
+         bcrypt.compare(password, user.password)
+            .then(isMatch => {
+               if (isMatch) {
+                  res.json({ msg: 'Success!' });
+               } else {
+                  return res.status(400).json({ password: 'Incorrect password.' });
+               }
+            });
+      });
+});
+
 module.exports = router;
